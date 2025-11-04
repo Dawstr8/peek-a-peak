23fd89c7fa0a001dc9ba6d6a8edc8267c6208a52
dawstr8 <dawid.strojek@gmail.com>
2025-11-04 09:13:43 +0000

    feat(frontend): make landing page accessible only to not logged-in users

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,3 +1,9 @@
-export default function Dashboard() {
+import { checkIfAuthenticated } from "@/lib/auth/utils";
+import { redirect } from "next/navigation";
+
+export default async function Dashboard() {
+  const isAuthenticated = await checkIfAuthenticated();
+  if (!isAuthenticated) redirect("/login");
+
   return <></>;
 }
