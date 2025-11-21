3b383970007b7fd98395c439291971c2740c78ed
dawstr8 <dawid.strojek@gmail.com>
2025-11-21 08:43:56 +0000

    refactor(frontend): use value in DateTimePicker

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -6,19 +6,34 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function latLngEqual(
+export const latLngEqual = (
   a: LatLng | undefined,
   b: LatLng | undefined,
-): boolean {
+): boolean => {
   if (!a && !b) return true;
   if (!a || !b) return false;
 
   return a.equals(b);
-}
+};
 
-export function dateEqual(a: Date | undefined, b: Date | undefined): boolean {
+export const dateEqual = (
+  a: Date | undefined,
+  b: Date | undefined,
+): boolean => {
   if (!a && !b) return true;
   if (!a || !b) return false;
 
   return a.getTime() === b.getTime();
-}
+};
+
+export const getTimeFromDate = (date: Date | undefined): string => {
+  return date ? date.toTimeString().slice(0, 8) : "00:00:00";
+};
+
+export const combineDateAndTime = (date: Date, time: string): Date => {
+  const newDate = new Date(date);
+  const [hours, minutes, seconds] = time.split(":").map(Number);
+  newDate.setHours(hours, minutes, seconds || 0);
+
+  return newDate;
+};
