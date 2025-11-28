33d8f01fccdaa864432957b94225c66b8401a4a2
dawstr8 <dawid.strojek@gmail.com>
2025-11-28 17:06:53 +0000

    refactor(backend): returning owner_id if access by username granted

--- backend/src/users/service.py
+++ backend/src/users/service.py
@@ -4,39 +4,32 @@ from src.models import SortParams
 from src.peaks.repository import PeaksRepository
 from src.photos.models import SummitPhoto, SummitPhotoDate, SummitPhotoLocation
 from src.photos.repository import PhotosRepository
-from src.users.repository import UsersRepository
 
 
 class UsersService:
 
     def __init__(
         self,
-        users_repository: UsersRepository,
         photos_repository: PhotosRepository,
         peaks_repository: PeaksRepository,
     ):
-        self.users_repository = users_repository
         self.photos_repository = photos_repository
         self.peaks_repository = peaks_repository
 
     async def get_photos_by_user(
-        self, username: str, sort_params: Optional[SortParams] = None
+        self, owner_id: int, sort_params: Optional[SortParams] = None
     ) -> List[SummitPhoto]:
-        user = await self.users_repository.get_by_username(username)
         return await self.photos_repository.get_by_owner_id(
-            user.id, sort_params=sort_params
+            owner_id, sort_params=sort_params
         )
 
     async def get_photos_locations_by_user(
-        self, username: str
+        self, owner_id: int
     ) -> List[SummitPhotoLocation]:
-        user = await self.users_repository.get_by_username(username)
-        return await self.photos_repository.get_locations_by_owner_id(user.id)
+        return await self.photos_repository.get_locations_by_owner_id(owner_id)
 
-    async def get_photos_dates_by_user(self, username: str) -> List[SummitPhotoDate]:
-        user = await self.users_repository.get_by_username(username)
-        return await self.photos_repository.get_dates_by_owner_id(user.id)
+    async def get_photos_dates_by_user(self, owner_id: int) -> List[SummitPhotoDate]:
+        return await self.photos_repository.get_dates_by_owner_id(owner_id)
 
-    async def get_summited_peaks_count_by_user(self, username: str) -> int:
-        user = await self.users_repository.get_by_username(username)
-        return await self.peaks_repository.get_summited_by_user_count(user.id)
+    async def get_summited_peaks_count_by_user(self, owner_id: int) -> int:
+        return await self.peaks_repository.get_summited_by_user_count(owner_id)
--- backend/tests/users/test_users_service.py
+++ backend/tests/users/test_users_service.py
@@ -5,30 +5,23 @@ from src.users.service import UsersService
 
 
 @pytest.fixture
-def users_service(
-    mock_users_repository, mock_photos_repository, mock_peaks_repository
-) -> UsersService:
-    return UsersService(
-        mock_users_repository, mock_photos_repository, mock_peaks_repository
-    )
+def users_service(mock_photos_repository, mock_peaks_repository) -> UsersService:
+    return UsersService(mock_photos_repository, mock_peaks_repository)
 
 
 @pytest.mark.asyncio
 async def test_get_photos_by_user(
     users_service,
-    mock_users_repository,
     mock_photos_repository,
     mock_photos,
     mock_user,
 ):
     user_id = mock_user.id
-    username = mock_user.username
 
-    result = await users_service.get_photos_by_user(username)
+    result = await users_service.get_photos_by_user(user_id)
 
     expected_photos = [photo for photo in mock_photos if photo.owner_id == user_id]
     assert result == expected_photos
-    mock_users_repository.get_by_username.assert_called_once_with(username)
     mock_photos_repository.get_by_owner_id.assert_called_once_with(
         user_id, sort_params=None
     )
@@ -37,15 +30,13 @@ async def test_get_photos_by_user(
 @pytest.mark.asyncio
 async def test_get_photos_locations_by_user(
     users_service,
-    mock_users_repository,
     mock_photos_repository,
     mock_photos,
     mock_user,
 ):
     user_id = mock_user.id
-    username = mock_user.username
 
-    result = await users_service.get_photos_locations_by_user(username)
+    result = await users_service.get_photos_locations_by_user(user_id)
 
     expected_locations = [
         SummitPhotoLocation(id=photo.id, lat=photo.lat, lng=photo.lng, alt=photo.alt)
@@ -53,7 +44,6 @@ async def test_get_photos_locations_by_user(
         if photo.owner_id == user_id
     ]
     assert result == expected_locations
-    mock_users_repository.get_by_username.assert_called_once_with(username)
     mock_photos_repository.get_locations_by_owner_id.assert_called_once_with(user_id)
 
 
@@ -66,9 +56,8 @@ async def test_get_photos_dates_by_user(
     mock_user,
 ):
     user_id = mock_user.id
-    username = mock_user.username
 
-    result = await users_service.get_photos_dates_by_user(username)
+    result = await users_service.get_photos_dates_by_user(user_id)
 
     expected_dates = [
         SummitPhotoDate(id=photo.id, captured_at=photo.captured_at)
@@ -76,19 +65,16 @@ async def test_get_photos_dates_by_user(
         if photo.owner_id == user_id
     ]
     assert result == expected_dates
-    mock_users_repository.get_by_username.assert_called_once_with(username)
     mock_photos_repository.get_dates_by_owner_id.assert_called_once_with(user_id)
 
 
 @pytest.mark.asyncio
 async def test_get_summited_by_user_count(
-    users_service, mock_users_repository, mock_peaks_repository, mock_user
+    users_service, mock_peaks_repository, mock_user
 ):
     user_id = mock_user.id
-    username = mock_user.username
 
-    count = await users_service.get_summited_peaks_count_by_user(username)
+    count = await users_service.get_summited_peaks_count_by_user(user_id)
 
     assert count == 2
-    mock_users_repository.get_by_username.assert_called_once_with(username)
     mock_peaks_repository.get_summited_by_user_count.assert_called_once_with(user_id)
