2f88f31a5453c0b1b95865eda73e6171d4feeda7
dawstr8 <dawid.strojek@gmail.com>
2025-12-02 13:21:47 +0000

    refactor(frontend): create RequireAuth component

--- frontend/src/app/(logged-in-user)/layout.tsx
+++ frontend/src/app/(logged-in-user)/layout.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import { redirect, usePathname } from "next/navigation";
-
-import { useAuth } from "@/components/auth/auth-context";
+import { RequireAuth } from "@/components/auth";
 import Footer from "@/components/layout/footer";
 import { Sidebar } from "@/components/layout/sidebar";
 import UploadDialog from "@/components/layout/upload-dialog";
@@ -18,31 +16,23 @@ export default function DashboardLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const { user, isLoading } = useAuth();
-
-  const pathname = usePathname();
-
-  if (!isLoading && !user) {
-    const searchParams = new URLSearchParams();
-    searchParams.set("returnTo", pathname);
-    redirect(`/login?${searchParams.toString()}`);
-  }
-
   return (
-    <UploadDialogProvider>
-      <SidebarProvider>
-        <Sidebar />
-        <SidebarInset>
-          <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-            <div className="px-4">
-              <SidebarTrigger />
-            </div>
-          </header>
-          <main className="container mx-auto flex-1 p-10">{children}</main>
-          <Footer />
-        </SidebarInset>
-      </SidebarProvider>
-      <UploadDialog />
-    </UploadDialogProvider>
+    <RequireAuth>
+      <UploadDialogProvider>
+        <SidebarProvider>
+          <Sidebar />
+          <SidebarInset>
+            <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+              <div className="px-4">
+                <SidebarTrigger />
+              </div>
+            </header>
+            <main className="container mx-auto flex-1 p-10">{children}</main>
+            <UploadDialog />
+            <Footer />
+          </SidebarInset>
+        </SidebarProvider>
+      </UploadDialogProvider>
+    </RequireAuth>
   );
 }
