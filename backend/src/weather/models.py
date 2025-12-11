2272569ac0269ef98730f5d9b4123c5d08d1ec1b
dawstr8 <dawid.strojek@gmail.com>
2025-12-11 09:52:39 +0000

    refactor(backend): make WeatherRecord's fields optional

--- backend/src/weather/models.py
+++ backend/src/weather/models.py
@@ -46,37 +46,41 @@ class WeatherRecord(SQLModel, table=True):
     )
 
     # Solar times
-    sunrise: datetime = Field(default=None, description="Sunrise time, UTC")
-    sunset: datetime = Field(default=None, description="Sunset time, UTC")
+    sunrise: Optional[datetime] = Field(default=None, description="Sunrise time, UTC")
+    sunset: Optional[datetime] = Field(default=None, description="Sunset time, UTC")
 
     # Temperature measurements
-    temp: float = Field(default=None, description="Temperature in Celsius")
-    feels_like: float = Field(
+    temp: Optional[float] = Field(default=None, description="Temperature in Celsius")
+    feels_like: Optional[float] = Field(
         default=None,
         description="Temperature in Celsius perceived by humans",
     )
-    dew_point: float = Field(
+    dew_point: Optional[float] = Field(
         default=None, description="Atmospheric temperature in Celsius"
     )
 
     # Atmospheric conditions
-    pressure: int = Field(
+    pressure: Optional[int] = Field(
         default=None, description="Atmospheric pressure on the sea levelin hPa"
     )
-    humidity: int = Field(default=None, description="Humidity percentage")
-    clouds: int = Field(default=None, description="Cloudiness percentage")
-    visibility: int = Field(default=None, description="Average visibility in meters")
+    humidity: Optional[int] = Field(default=None, description="Humidity percentage")
+    clouds: Optional[int] = Field(default=None, description="Cloudiness percentage")
+    visibility: Optional[int] = Field(
+        default=None, description="Average visibility in meters"
+    )
 
     # Wind conditions
-    wind_speed: float = Field(default=None, description="Wind speed in meter/sec")
-    wind_deg: int = Field(
+    wind_speed: Optional[float] = Field(
+        default=None, description="Wind speed in meter/sec"
+    )
+    wind_deg: Optional[int] = Field(
         default=None, description="Wind direction in degrees (meteorological)"
     )
 
     # Precipitation
-    rain: float = Field(
+    rain: Optional[float] = Field(
         default=None, description="Precipitation for the last 1 hour in mm/h"
     )
-    snow: float = Field(
+    snow: Optional[float] = Field(
         default=None, description="Precipitation for the last 1 hour in mm/h"
     )
